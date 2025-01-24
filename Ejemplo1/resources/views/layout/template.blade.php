7c91aac49e07be86e1113c52ecc41f10f6914cb5
Lacran11 <lacran119@gmail.com>
2025-01-24 12:01:31 -0600

    Update 2

--- Ejemplo1/resources/views/layout/template.blade.php
+++ Ejemplo1/resources/views/layout/template.blade.php
@@ -7,12 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('Titulo')</title>
      <!-- Styles / Scripts -->
-     {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
      <style>
-        {!! Vite::content('resources/css/app.css') !!}
+        /* {!! Vite::content('resources/css/app.css') !!} */
     </style>
     <script>
-        {!! Vite::content('resources/js/app.js') !!}
+        /* {!! Vite::content('resources/js/app.js') !!} */
 
         window.Laravel = {!! json_encode([
             'alert' => session('alert')
